ee502416c82538f59f7c83fd556041b55f2da22e
naivetang <43596144+naivetang@users.noreply.github.com>
2019-03-19 23:32:11 +0800

    Server

---
+++ Server/Model/DataBase/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ETModel
+{
+    [BsonIgnoreExtraElements]
+    public class Account:ETModel.Entity
+    {
+        public string UserName;
+
+        public string Password;
+    }
+}
