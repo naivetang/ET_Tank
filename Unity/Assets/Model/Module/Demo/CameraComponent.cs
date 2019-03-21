433953ab0efa502c339caaf7a037e8fdb6d2c95e
naivetang <43596144+naivetang@users.noreply.github.com>
2019-03-21 23:18:07 +0800

    相机转动和跟随

--- Unity/Assets/Model/Module/Demo/CameraComponent.cs
+++ Unity/Assets/Model/Module/Demo/CameraComponent.cs
@@ -1,4 +1,8 @@
-﻿using UnityEngine;
+﻿using System;
+using PF;
+using UnityEngine;
+using Mathf = UnityEngine.Mathf;
+using Vector3 = UnityEngine.Vector3;
 
 namespace ETModel
 {
@@ -25,7 +29,32 @@ namespace ETModel
 		// 战斗摄像机
 		public Camera mainCamera;
 
-		public Unit Unit;
+		public Tank tank;
+
+        //距离
+        private float distance = 10f;
+
+        //横向角度
+        private float rot = 0;
+
+        // 纵向角度范围
+        private  float maxRoll = 60f * Mathf.PI * 2 / 360;
+        private float minRoll = 10f * Mathf.PI * 2 / 360;
+
+        //纵向角度
+        private float roll = 30f * Mathf.PI * 2 / 360;
+
+        //横向旋转角度
+        private float rotSpeed = 0.2f;
+
+        // 距离范围
+        private float maxDistance = 20f;
+        private float minDistance = 5f;
+
+        //距离变化的速度
+        private float zoomSpeed = 0.2f;
+
+
 
 		public Camera MainCamera
 		{
@@ -38,7 +67,8 @@ namespace ETModel
 		public void Awake()
 		{
 			this.mainCamera = Camera.main;
-		}
+            this.tank = this.GetParent<Tank>();
+        }
 
 		public void LateUpdate()
 		{
@@ -47,9 +77,51 @@ namespace ETModel
 		}
 
 		private void UpdatePosition()
-		{
-			Vector3 cameraPos = this.mainCamera.transform.position;
-			this.mainCamera.transform.position = new Vector3(this.Unit.Position.x, cameraPos.y, this.Unit.Position.z - 1);
-		}
+        {
+            Vector3 targetPos = this.tank.Position;
+
+            Vector3 cameraPos;
+
+            float d = this.distance * Mathf.Cos(roll);
+
+            float height = this.distance * Mathf.Sin(roll);
+
+            cameraPos.x = targetPos.x + d * Mathf.Cos(rot);
+
+            cameraPos.y = targetPos.y + height;
+
+            cameraPos.z = targetPos.z + d * Mathf.Sin(rot);
+
+            this.MainCamera.transform.position = cameraPos;
+
+            this.MainCamera.transform.LookAt(this.tank.GameObject.transform);
+
+            Rotate();
+
+            return;
+
+			//Vector3 cameraPos = this.mainCamera.transform.position;
+			this.mainCamera.transform.position = new Vector3(this.tank.Position.x , this.tank.Position.y + 5, this.tank.Position.z - 6);
+            this.mainCamera.transform.LookAt(this.tank.Position);
+			
+			//this.mainCamera.transform.eulerAngles = new Vector3(30,0,0);
+            
+        }
+
+        // 鼠标控制相机旋转
+        private void Rotate()
+        {
+            float x = Input.GetAxis("Mouse X") * this.rotSpeed;
+
+            this.rot -= x;
+
+            float y = Input.GetAxis("Mouse Y") * this.rotSpeed;
+
+            this.roll -= y;
+
+            this.roll = PF.Mathf.Clamp(this.roll, this.minRoll, this.maxRoll);
+
+            //Log.Info($"roll = {roll}" );
+        }
 	}
 }
