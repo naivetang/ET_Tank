ebd0f2eab50fd31a414bd414c52006a8fc4ee9c8
naivetang <43596144+naivetang@users.noreply.github.com>
2019-03-21 23:18:27 +0800

    删除注释

--- Unity/Assets/Hotfix/UI/GM/FUIGMComponent.cs
+++ Unity/Assets/Hotfix/UI/GM/FUIGMComponent.cs
@@ -28,13 +28,7 @@ namespace ETHotfix
         {
             string cmd =  self.Input.GObject.asCom.GetChild("n1").asTextInput.text;
 
-            long a = Convert.ToInt64(cmd);
-
-            Log.Warning("cmd = " + cmd);
-
             string[] cmds = cmd.Split(' ');
-
-            Log.Info("长度 = " + cmds.Length);
 
             if (cmds.Length == 1)
             {
@@ -42,9 +36,6 @@ namespace ETHotfix
             }
             else if(cmds.Length == 2)
             {
-                Log.Info("正确进入");
-                Log.Warning("cmd = " + cmds[0]);
-                Log.Warning("参数 = " + cmds[1]);
 
                 Game.EventSystem.Run(cmds[0],(object)cmds[1]);
             }
